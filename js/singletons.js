2a45ce959d99a853327c8a4c7d8f7b252d2eb231
IRIS <2212082@slu.edu.ph>
2025-11-19 03:12:35 +0800

    added one singleton

--- js/singletons.js
+++ js/singletons.js
@@ -1,3 +1,6 @@
 //Singletons: single instance objects. This is to ensure that the data being used is consistent.
 
 //TODO: create and instance of each created modules/ classes.
+import { PrescriptionObject } from "./prescriptionObject";
+
+export const prescriptObj = new PrescriptionObject();
