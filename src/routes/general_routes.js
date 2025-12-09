3db744caa8aeac69b0ace5bda3e4d4055f0f2f29
XYMOND LOUISSE M. ALCAZAR <alcazarxymondlouisse@gmail.com>
2025-12-10 06:09:41 +0800

    added some contents and dependencies.

---
+++ src/routes/general_routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+//TODO: Routes that all modules can be used (i.e. user details from db and others)
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,1 +1,11 @@
-//TODO: Entry point of the application
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+//entry points here
+
+//Server Start
+app.listen(PORT, () => {
+  console.log(`Server is running on [http://localhost:${PORT}]`);
+});
