0d249bec7c423d99469168eaa3808cb07fd030bb
IRIS <2212082@slu.edu.ph>
2025-12-15 19:19:16 +0800

    delete old admin-module

--- admin-module/backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const authControl = require('../controllers/authCon');
-
-const route_point = express.Router();
-
-route_point.post('/login', authControl.login);
-route_point.post('/logout', authControl.logout);
-
-module.exports = route_point;
